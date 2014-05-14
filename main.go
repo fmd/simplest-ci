223ba152923bfee8cc3f43ea892d4ea3f6dfeaa1
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-05-14 18:09:05 +0000

    cmds

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
     "github.com/go-martini/martini"
     "github.com/fsouza/go-dockerclient"
     "github.com/garyburd/redigo/redis"
+    "encoding/json"
     "net/http"
     "time"
     "log"
     "fmt"
+    "os"
 )
 
 var version string = "0.0.1"
@@ -16,15 +18,22 @@ var version string = "0.0.1"
 func usage() string {
     return `simplest-ci
         Usage:
-            simplest-ci [--redis=<redis>] [--port=<port>]
+            simplest-ci daemon [--redis=<redis>] [--port=<port>]
+            simplest-ci run [--config=<config>]
             simplest-ci --help
             simplest-ci --version
 
         Options:
-            -p | --port   The port to listen on [default: 5000].
-            -r | --redis  The redis host string [default: localhost:6379].
-            --help        Show this screen.
-            --version     Show version.`
+            -p | --port    The port to listen on.         [default: 5000]
+            -r | --redis   The redis host string.         [default: localhost:6379]
+            -c | --config  The config file to serve from. [default: simplest-config.json]
+            --help         Show this screen.
+            --version      Show version.`
+}
+
+type Config struct {
+    Commit   string     `json:"commit"`
+    Commands [][]string `json:"commands"`
 }
 
 type Simplest struct {
@@ -32,25 +41,68 @@ type Simplest struct {
     Docker *docker.Client
 }
 
-func main() {
+//runFromConfig takes a simplest config file and uses it to
+//Update git, check out the correct commit, build the project and run the tests.
+func runFromConfig(configFile string) error {
+    file, err := os.Open(configFile)
+    if err != nil {
+        return err
+    }
+
+    conf := &Config{}
+
+    parser := json.NewDecoder(file)
+    err = parser.Decode(&conf)
+    if err != nil {
+        return err
+    }
+
+    fmt.Println(conf)
+
+    return nil
+}
+
+//serveFromConfig starts a daemon uses redis and docker to start up instances of a project from testing.
+func serveFromConfig(redisHost string, servePort string) {
+
     var err error
-    args, _ := docopt.Parse(usage(), nil, true, fmt.Sprintf("simplest-ci %s", version), false)
 
-    redisHost := args["--redis"].(string)
-    servePort := args["--port"].(string)
-
+    //Create our main struct
     s := &Simplest{}
+
+    //Connect to redis
     s.Redis, err = redis.DialTimeout("tcp", redisHost, 0, 1*time.Second, 1*time.Second)
     if err != nil {
         panic(err)
     }
 
+    //Connect to docker
     endpoint := "unix:///var/run/docker.sock"
     s.Docker, err = docker.NewClient(endpoint)
     if err != nil {
         panic(err)
     }
 
+    //Drink a martini
     m := martini.Classic()
     log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s",servePort), m))
+}
+
+func main() {
+
+    //Parse args
+    args, _ := docopt.Parse(usage(), nil, true, fmt.Sprintf("simplest-ci %s", version), false)
+
+    //See what we're doing.
+    if args["run"].(bool) {
+        runFromConfig(args["--config"].(string))
+    } else {
+        redisHost := args["--redis"].(string)
+        servePort := args["--port"].(string)
+        serveFromConfig(redisHost, servePort)
+    }
+}
+
+func (s *Simplest) newInstance(image string, path string, commit string) {
+
 }
